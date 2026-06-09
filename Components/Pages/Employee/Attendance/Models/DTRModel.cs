484a34e8947827f7fd717338a1528b4fea5167ac
James Fanuel Damaso <101571191+James54-cmd@users.noreply.github.com>
2026-06-09 14:27:30 +0800

    feat: implement DTR model and display component for employee attendance records

---
+++ Components/Pages/Employee/Attendance/Models/DTRModel.cs
@@ -0,0 +1,12 @@
+namespace EduPanel.Components.Pages.Employee.Attendance.Models;
+
+public class DTRModel
+{
+    public string TransactionNo { get; set; } = "";
+    public string Fpid { get; set; } = "0";
+    public string DtrDate { get; set; } = "";
+    public string LogType { get; set; } = "";
+    public string DtrTime { get; set; } = "";
+    public string MachineName { get; set; } = "";
+    public string Status { get; set; } = "For Downloading";
+}
