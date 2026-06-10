04093071641323abaa22e7ed4c3eb78fd1c85d14
James Fanuel Damaso <101571191+James54-cmd@users.noreply.github.com>
2026-06-10 15:50:26 +0800

    feat: implement Notice to Explain module and integrate into Human Resources page

---
+++ Components/Pages/Employee/HumanResource/Models/NoticeToExplainModel.cs
@@ -0,0 +1,10 @@
+namespace EduPanel.Components.Pages.Employee.HumanResource.Models
+{
+    public class NoticeToExplainModel
+    {
+        public string ReportNo { get; set; } = "";
+        public string DateIssued { get; set; } = "";
+        public string DaPolicyType { get; set; } = "";
+        public string DaPolicy { get; set; } = "";
+    }
+}
