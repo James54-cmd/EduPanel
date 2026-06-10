9b037f9ced76d484fc46d94e4335e0c56d3b37dc
James Fanuel Damaso <101571191+James54-cmd@users.noreply.github.com>
2026-06-10 16:16:41 +0800

    feat: implement HR My Applications page with dynamic routing and data table view

---
+++ Components/Pages/Employee/HumanResource/Models/MyApplicationModel.cs
@@ -0,0 +1,10 @@
+namespace EduPanel.Components.Pages.Employee.HumanResource.Models
+{
+    public class MyApplicationModel
+    {
+        public string Status { get; set; } = "";
+        public string PositionTitle { get; set; } = "";
+        public string DateApplied { get; set; } = "";
+        public string ApplicantStatus { get; set; } = "";
+    }
+}
